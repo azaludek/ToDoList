6122954f3c9ab055d25921588f75f32b5a7a712e
Adam Žalůdek <104594328+azaludek@users.noreply.github.com>
2023-02-26 13:16:44 +0100

    feat: add and delete tasks, checkbox

--- script.js
+++ script.js
@@ -1,13 +1,36 @@
 let container = document.getElementsByClassName('container');
-
 let ul = document.createElement("ul");
 container[0].appendChild(ul);
 
+let taskNumber = 1;
+
 function Pridat() {
-    let input = document.getElementById('input').value;
-    for (let i = 0; i < 1; i++) {
-        let li = document.createElement("li");
-        li.appendChild(document.createTextNode(input));
-        ul.appendChild(li); 
-    }
+  let input = document.getElementById('input').value;
+  let li = document.createElement("li");
+  
+  let checkbox = document.createElement("input");
+  checkbox.type = "checkbox";
+  checkbox.setAttribute("id", "task" + taskNumber);
+  
+  li.appendChild(document.createTextNode(taskNumber + ". " + input));
+  li.appendChild(checkbox);
+  ul.appendChild(li);
+  
+  taskNumber++;
 }
+
+function Smazat() {
+  let checkboxes = document.querySelectorAll('input[type="checkbox"]');
+  for (let i = 0; i < checkboxes.length; i++) {
+    if (checkboxes[i].checked) {
+      let li = checkboxes[i].parentElement;
+      ul.removeChild    (li);
+    }
+  }
+}
+
+let deleteButton = document.createElement("button");
+deleteButton.innerText = "Smazat vybrané";
+deleteButton.setAttribute("id", "delete");
+deleteButton.onclick = Smazat;
+container[0].appendChild(deleteButton);
