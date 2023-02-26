22e6ca26eda43b827f447a7a0a6afe564a208cc7
Adam Žalůdek <104594328+azaludek@users.noreply.github.com>
2023-02-26 12:51:58 +0100

    idk

--- script.js
+++ script.js
@@ -1,18 +1,7 @@
 let container = document.getElementsByClassName('container');
-container[0].style.backgroundColor = "teal";
-
-let par = document.createElement("p");
-container[0].appendChild(par).innerHTML = "Odstavec";
-
-let heading = document.createElement("h1");
-heading.setAttribute("class", "nadpis");
-heading.setAttribute("onclick", "Test()")
-container[0].appendChild(heading).innerHTML = "Další velký nadpis";
 
 let ul = document.createElement("ul");
 container[0].appendChild(ul);
-
-//let umisteni = document.getElementsByTagName("ul");
 
 function Pridat() {
     let input = document.getElementById('input').value;
